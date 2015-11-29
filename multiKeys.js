e79943ae0804264d84d1a02b276b58ac61b30e69
jonaDuran <jonyei.duran30@gmail.com>
2015-11-29 08:50:45 -0600

    Documentar
    quitar detalles innecesarios
    documentar

--- multiKeys.js
+++ multiKeys.js
@@ -1,30 +1,32 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function load(){
+// carga las variables
+function load() {
+    
+    // arreglo de elementos, cada uno corresponde al id de una <figure/>
     key = [up, down, left, right, keyI, keyO, keyP, keyK, keyL, keyN];
+    
+    // arreglo para almacenar que tecla se está precionando
     down = [key.length];
+    down.fill(false);
     
-    fondoDown = "#ccc";
-    fondoUp = "#eee";
+    // colores de las teclas
+    backgroundDown = "#ccc";
+    backgroundUp = "#eee";
     
-    for(x = 0; x < key.length; x++)
-        down[x] = false;
     change();
 }
 
-function change(){
-    for(x = 0; x < key.length; x++){
+// actualiza el color de las teclas
+function change() {
+    for(x = 0; x < key.length; x++) {
         if(down[x] === true)
-            key[x].style.background = fondoDown;
+            key[x].style.background = backgroundDown;
         else
-            key[x].style.background = fondoUp;
+            key[x].style.background = backgroundUp;
     }
 }
 
-function charKey(char){
+// retorna la posición respectiva a la tecla en el down[]
+function charKey(char) {
     switch (char){
         case "W": return 0;
         case "S": return 1;
@@ -35,25 +37,31 @@ function charKey(char){
         case "P": return 6;
         case "K": return 7;
         case "L": return 8;
-        case "º": return 9; // Ñ
+        case "º": return 9;  // Ñ teclado español
+        case "À": return 9;  // Ñ teclado latinoamérica
     }
 }
 
-function keyDown(evt){
-    var key = String.fromCharCode(evt.keyCode);
+// lee la tecla presionada
+function keyDown(evt) {
+    var key = String.fromCharCode(evt.keyCode); 
     down[charKey(key)] = true;
+    console.log(key);
     change();
-    //alert (key);
 }
 
+// lee la tecla que se dejó de presionar
 function keyUp(evt){
     var key = String.fromCharCode(evt.keyCode);
     down[charKey(key)] = false;
     change();
 }
 
-window.onkeydown = keyDown;
 
-window.onkeyup = keyUp;
+/* se asignan las funciones a los eventos */
 
-window.onload = load;
+window.onkeydown = keyDown;     // cuando se preciona una tecla
+
+window.onkeyup = keyUp;         // cuando se deja de precionar una tecla
+
+window.onload = load;           // cuando se termina de cargar la página
